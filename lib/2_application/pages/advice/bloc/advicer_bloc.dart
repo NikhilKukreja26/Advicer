f4771960340c1c95d43eff8946a3f080cc05c257
NikhilKukreja26 <kukrejanikhil25@gmail.com>
2023-10-23 17:11:45 +0530

    [feature] - AdviseRequestedEvent Created

--- lib/2_application/pages/advice/bloc/advicer_bloc.dart
+++ lib/2_application/pages/advice/bloc/advicer_bloc.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 
@@ -6,8 +7,15 @@ part 'advicer_state.dart';
 
 class AdvicerBloc extends Bloc<AdvicerEvent, AdvicerState> {
   AdvicerBloc() : super(AdvicerInitial()) {
-    on<AdvicerEvent>((event, emit) {
-      // TODO: implement event handler
+    on<AdviceRequestedEvent>((event, emit) async {
+      emit(AdvicerStateLoading());
+      // executes the business logic
+      // for example get and advise
+      debugPrint('fake get advise triggered');
+      await Future.delayed(const Duration(seconds: 3), () {});
+      debugPrint('got advise');
+      emit(const AdvicerStateLoaded(advice: 'fake advise to test bloc'));
+      emit(const AdvicerStateError(message: 'error message'));
     });
   }
 }
